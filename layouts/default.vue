3adad8c577cbf2e213e3755d97c9eac335e97570
xlanex6 <alexduval71@gmail.com>
2024-05-07 11:19:04 +0200

    add pwa

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,10 @@
 <template>
-<div class="min-h-screen bg-red-50 relative">
-  <TopMenu class=""/>
-  <NuxtPage class="my-12"/>
-  <BottomMenu class="fixed bottom-0"/>
-</div>
+  <div class="min-h-screen bg-red-50 relative">
+    <NuxtPwaManifest />
+    <TopMenu class="" />
+    <NuxtPage class="my-12" />
+    <BottomMenu class="fixed bottom-0" />
+  </div>
 </template>
 
 <script setup>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,45 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss","nuxt-icon"]
+  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@vite-pwa/nuxt"],
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: "Burger Drive",
+      short_name: "BurgerDrive",
+      description: "Burge Drive app",
+      lang: "en",
+      theme_color: "#FF0000",
+      icons: [
+        {
+          src: 'android-launchericon-192-192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'android-launchericon-512-512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'android-launchericon-512-512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    injectManifest: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    client: {
+      installPrompt: true,
+      // you don't need to include this: only for testing purposes
+      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+      // periodicSyncForUpdates: 20,
+    },
+  }
 })
